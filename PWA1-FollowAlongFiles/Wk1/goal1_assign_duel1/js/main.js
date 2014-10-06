08304e4c5218086914fb827ebbe794761580fb9b
John Smith <rjdj7@yahoo.com>
2014-10-06 15:02:21 -0400

    Starting JavaScript Pseudocode for Duel 1 assignment

---
+++ PWA1-FollowAlongFiles/Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,19 @@
+// John Smith
+// 10/06/14
+// Assignment: Goal 1: Analyze Duel #1
+
+//PSEUDOCODE
+
+//Setup a (self-executing?) function:
+//		that has variables inside for both players' Names, 
+//		Max-Damage, and 
+//		Starting Health 
+//		as well as a variable for the current Round
+//
+//
+//	Setup a function within it named "fight" that loops through rounds and reduces the player’s health accordingly
+//  	
+//  	
+//		
+//		
+//	Setup another function named "winnerCheck" that does the conditional logic to determine if there is a winner or not.
