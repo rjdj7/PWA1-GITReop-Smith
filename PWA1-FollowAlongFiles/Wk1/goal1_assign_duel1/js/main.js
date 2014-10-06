8b16d60ec703b22a63244282c95ba229f1a3a647
John Smith <rjdj7@yahoo.com>
2014-10-06 15:24:49 -0400

    Pseudocode 2nd Commit

--- PWA1-FollowAlongFiles/Wk1/goal1_assign_duel1/js/main.js
+++ PWA1-FollowAlongFiles/Wk1/goal1_assign_duel1/js/main.js
@@ -4,16 +4,40 @@
 
 //PSEUDOCODE
 
-//Setup a (self-executing?) function:
-//		that has variables inside for both players' Names, 
-//		Max-Damage, and 
-//		Starting Health 
-//		as well as a variable for the current Round
+//Setup a (self-executing?) function{
+//		that has variables inside for both players' Names;
+//		Max-Damage, and;
+//		Starting Health;
+//		as well as a variable for the current Round;
 //
 //
-//	Setup a function within it named "fight" that loops through rounds and reduces the player’s health accordingly
-//  	
-//  	
+//	Setup a function within it named "fight" that loops through rounds and reduces the player’s health accordingly{
+//		Setup an alert that shows the first player's name and health, the round number, and the second player's name and health;
+//
+//  	Create a (for) loop that will run 10 rounds{
 //		
-//		
-//	Setup another function named "winnerCheck" that does the conditional logic to determine if there is a winner or not.
+//		};
+//  	
+//		Somewhere in here have it call the winnerCheck function;
+//	};	
+//	Setup another function named "winnerCheck" that does the conditional logic to determine if there is a winner or not{
+//
+//
+//
+//	};
+//
+//
+//	Call the "fight" function;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
