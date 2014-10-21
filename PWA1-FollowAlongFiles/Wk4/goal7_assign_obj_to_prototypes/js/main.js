06d56fd158e9031090ba674cf49b730665df399f
John Smith <rjdj7@yahoo.com>
2014-10-21 16:32:38 -0400

    Goal 7 Completed

--- PWA1-FollowAlongFiles/Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1-FollowAlongFiles/Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -2,5 +2,17 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
- */
+ *
+
+ Create the array, "names", with 5 people names[name1, name2, name3, name4, name5];
+ Create the array "people" and store 3 objects that are calling the Person constructor[obj1, obj2, obj3];
+
+ Math.random(names) < choosing a random name from the "names" array; v
+
+Create "person" variable and store that random name in it.
+
+
+
+
+
 
--- PWA1-FollowAlongFiles/Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ PWA1-FollowAlongFiles/Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -2,4 +2,19 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
- */
+ *
+
+ Create variable "jobs";
+ Create variable "actions";
+
+ Creat a "jobs" array[teacher, farmer, student, pilot];
+
+ Create an "actions" array[working, sleeping, eating];
+
+Create "Person" contructor [
+	name:
+	action:
+	job:
+	row:
+];
+
